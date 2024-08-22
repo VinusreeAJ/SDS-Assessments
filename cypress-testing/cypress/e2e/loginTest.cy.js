ada993bf70bb059e42c2947fe2f6307ea55af8d8
VinusreeAJ <vinusree.j@arkinfotec.com>
2024-08-22 13:09:25 +0530

    test cases updated

--- cypress-testing/cypress/e2e/loginTest.cy.js
+++ cypress-testing/cypress/e2e/loginTest.cy.js
@@ -5,7 +5,30 @@ describe('Login Form Validation', () => {
     cy.wait(2000);
   });
 
-  it('displays error messages when form is submitted without filling required fields', () => {
+  it('displays error message when form is submitted without password field', () => {
+
+    // Type into the Email field
+    cy.get('#email').type('example.x@gmail.com');
+    cy.wait(2000);
+    cy.get('#email').should('have.value', 'example.x@gmail.com');
+
+    // Attempt to submit the form without entering values
+    cy.get('button[type="submit"]').click();
+    cy.wait(2000);
+
+    // Check that the password error message is displayed
+    cy.get('.text-danger')
+      .should('be.visible')
+      .and('contain.text', 'Password is required');
+    cy.wait(2000);
+  });
+
+  it('displays error message when form is submitted without email field', () => {
+
+    // Type into the password field
+    cy.get('#password').type('password123');
+    cy.wait(2000);
+    cy.get('#password').should('have.value', 'password123');
 
     // Attempt to submit the form without entering values
     cy.get('button[type="submit"]').click();
@@ -16,24 +39,6 @@ describe('Login Form Validation', () => {
       .should('be.visible')
       .and('contain.text', 'Email is required');
     cy.wait(2000);
-
-    // Check that the password error message is displayed
-    cy.get('.text-danger')
-      .should('be.visible')
-      .and('contain.text', 'Password is required');
-  });
-
-  it('check Email and password fields are getting values', () => {
-
-    // Type into the Email field
-    cy.get('#email').type('example.x@gmail.com');
-    cy.wait(2000);
-    cy.get('#email').should('have.value', 'example.x@gmail.com');
-
-    // Type into the password field
-    cy.get('#password').type('password123');
-    cy.wait(2000);
-    cy.get('#password').should('have.value', 'password123');
   });
 
   it('displays success message when form is submitted with valid email and password', () => {
